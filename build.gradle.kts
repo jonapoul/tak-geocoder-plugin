b561b63fd47ee239720b36fad373b5103b4beb0f
Jon Poulton <jpoulton@protonmail.com>
2023-08-09 07:17:59 +0100

    Reshuffled plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,57 @@
-/**
- * Only using buildscript here so we can handle branching logic of TakDev plugin coordinates
- */
 buildscript {
+  val isPipeline = properties["takrepo.url"] != null
+
+  extra.apply {
+    set("takrepoUrl", project.properties["takrepo.url"] ?: "http://localhost/")
+    set("takrepoUser", project.properties["takrepo.user"] ?: "invalid")
+    set("takrepoPassword", project.properties["takrepo.password"] ?: "invalid")
+    set("takdevPlugin", project.properties["takrepo.plugin"] ?: "${rootDir}/../../atak-gradle-takdev.jar")
+    set("takdevConTestEnable", false)
+    set("ATAK_VERSION", libs.versions.atak.get())
+    set("PLUGIN_VERSION", libs.versions.pluginVersionName.get())
+  }
+  println("Extras = ${extra.properties}")
+
+  fun Project.getStringProperty(key: String): String {
+    val value = project.properties[key]
+    return value as? String ?: error("Expected string from $key, got $value")
+  }
+
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    google()
+    maven { url = uri("https://jitpack.io") }
+    if (isPipeline) {
+      println("Using pipeline, declaring takRepo maven repo")
+      val takrepoUrl = getStringProperty(key = "takrepoUrl")
+      val takrepoUser = getStringProperty(key = "takrepoUser")
+      val takrepoPassword = getStringProperty(key = "takrepoPassword")
+      maven {
+        isAllowInsecureProtocol = true
+        url = uri(takrepoUrl)
+        credentials {
+          username = takrepoUser
+          password = takrepoPassword
+        }
+      }
+    } else {
+      println("Not using pipeline, declaring local maven repo")
+      maven { url = uri("${rootProject.projectDir}/maven") }
+    }
+  }
+
   dependencies {
-    val isPipeline = properties["takrepo.url"] != null
+    classpath(libs.plugin.agp)
+    classpath(libs.plugin.detekt)
+    classpath(libs.plugin.doctor)
+    classpath(libs.plugin.kotlin)
+    classpath(libs.plugin.kover)
+    classpath(libs.plugin.ktlint)
+    classpath(libs.plugin.licensee)
+    classpath(libs.plugin.serialization)
+    classpath(libs.plugin.spotless)
+
     if (isPipeline) {
       /* Full-fat plugin */
       classpath("com.atakmap.gradle:atak-gradle-takdev:2.+")
@@ -16,16 +64,6 @@ buildscript {
 }
 
 plugins {
-  alias(libs.plugins.agp.app) apply false
-  alias(libs.plugins.agp.lib) apply false
-  alias(libs.plugins.detekt) apply false
-  alias(libs.plugins.kotlin.android) apply false
-  alias(libs.plugins.kotlin.serialization) apply false
-  alias(libs.plugins.ktlint) apply false
-  alias(libs.plugins.kover) apply false
-  alias(libs.plugins.licensee) apply false
-  alias(libs.plugins.spotless) apply false
-
   alias(libs.plugins.doctor) // configured below
 
   id("convention-properties")
@@ -49,16 +87,6 @@ configurations.configureEach {
         .using(module(libs.plugin.proguard.get().toString()))
     }
   }
-}
-
-extra.apply {
-  set("takrepoUrl", project.getProperty("takrepo.url", "http://localhost/"))
-  set("takrepoUser", project.getProperty("takrepo.user", "invalid"))
-  set("takrepoPassword", project.getProperty("takrepo.password", "invalid"))
-  set("takdevPlugin", project.getProperty("takrepo.plugin", "${rootDir}/../../atak-gradle-takdev.jar"))
-  set("takdevConTestEnable", false)
-  set("ATAK_VERSION", libs.versions.atak.get())
-  set("PLUGIN_VERSION", libs.versions.pluginVersionName.get())
 }
 
 doctor {
